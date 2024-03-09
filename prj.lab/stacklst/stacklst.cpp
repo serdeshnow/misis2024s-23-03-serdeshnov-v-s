c516b8413fea24fe4d0d777275d3cde0c11055eb
serdeshnow <vserdes1977@mail.ru>
2024-03-10 00:40:29 +0300

    Update: StackLst

---
+++ prj.lab/stacklst/stacklst.cpp
@@ -0,0 +1,25 @@
+#include "stacklst.h"
+
+StackLst::Node* StackLst::FindLast() noexcept {
+
+}
+
+void StackLst::Push() {
+
+}
+
+void StackLst::Pop() {
+
+}
+
+Complex &StackLst::Top() {
+
+}
+
+void StackLst::Clear() {
+
+}
+
+bool StackLst::IsEmpty() {
+}
+
--- prj.lab/stacklst/stacklst.h
+++ prj.lab/stacklst/stacklst.h
@@ -1,29 +1,38 @@
 #ifndef STACKLST_LIB_HPP
 #define STACKLST_LIB_HPP
+
+#include "complex/complex.h"
+
 #include <cstdint>
+#include <stdexcept>
 
 
-class StackLst() {
+class StackLst {
 public:
-  StackLst() = default;
-  StackLst(const StackLst&) = default;
-  ~StackLst() = default;
+  StackLst() = default; // default
+  StackLst(const StackLst&) = default; // (0)
+  ~StackLst() = default; // (0)
 
+  void Push(); // (3)
 
+  void Pop(); // (4)
 
+  Complex& Top(); // (2.1)
 
+  const Complex& Top(); // (2.2)
 
+  void Clear(); // (5)
+
+  bool IsEmpty(); // (1)
 
 private:
 	struct Node {
-		int value_; // int --> int&
+		Complex& value_; // int --> int&
+
 		Node* next = nullptr; // указатель на следующий элемент
-
 	};
+  Node* FindLast(); // (2.0)
 	Node* head_ = nullptr;
-
-
-
 }
 
 #endif // !STACKLST_LIB_HPP
